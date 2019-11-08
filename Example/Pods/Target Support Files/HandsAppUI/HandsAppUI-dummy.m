52c84e1f8c0bef1f9e0e96cbceb9057cf0b064ca
Никита Красавин <6nike6@bk.ru>
2019-11-08 14:30:44 +0300

    delete example project

--- Example/Pods/Target Support Files/HandsAppUI/HandsAppUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HandsAppUI : NSObject
-@end
-@implementation PodsDummy_HandsAppUI
-@end
--- Example/Pods/Target Support Files/Pods-HandsAppUI_Tests/Pods-HandsAppUI_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HandsAppUI_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_HandsAppUI_Tests
-@end
